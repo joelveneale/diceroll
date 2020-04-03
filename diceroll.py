b3c6a1c08c958179d77071892981e031aaeeeeab
joelveneale <joelpneale@gmail.com>
2020-04-03 13:40:47 +0100

    second bug fix version

--- diceroll.py
+++ diceroll.py
@@ -7,12 +7,15 @@ rollcounter = 0
 while rollcounter < 10:
 
   input("Would you like to roll the dice? Y/N")
-  if input == 'Y' or 'y':
+  if input == 'Y':
      roll = random.randint(1,7)
      print("The dice roll was a " + str(roll))
-     rollcounter = rollcounter - 1
-  else:
+     rollcounter = rollcounter + 1
+  elif input == "N":
      print("Let me know when you are ready to roll.")
+  else:
+     print("Please enter Y or N to roll the dice")
+
 
 print("You have rolled the maximum number of times")
 
