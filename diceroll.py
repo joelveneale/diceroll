06196b2f70622569f68aa0c5f648cf95086ae5ed
joelveneale <joelpneale@gmail.com>
2020-04-03 13:35:52 +0100

    version one build

---
+++ diceroll.py
@@ -0,0 +1,18 @@
+import random
+
+
+print("Welcome to the virtual dice roller")
+
+rollcounter = 0
+while rollcounter < 10:
+
+  input("Would you like to roll the dice? Y/N")
+  if input = 'Y':
+     roll = random.randint(1,7)
+     print("The dice roll was a " + str(roll))
+     rollcounter = rollcounter - 1
+  else:
+     print("Let me know when you are ready to roll.")
+
+print("You have rolled the maximum number of times")
+
