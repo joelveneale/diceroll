55077c9e0922aef195e7d5cd94ffb997505f2834
joelveneale <joelpneale@gmail.com>
2020-04-03 13:43:02 +0100

    3rd version bug fixes

--- diceroll.py
+++ diceroll.py
@@ -6,8 +6,8 @@ print("Welcome to the virtual dice roller")
 rollcounter = 0
 while rollcounter < 10:
 
-  input("Would you like to roll the dice? Y/N")
-  if input == 'Y':
+  input("Would you like to roll the dice? Y/N: ")
+  if input == "Y":
      roll = random.randint(1,7)
      print("The dice roll was a " + str(roll))
      rollcounter = rollcounter + 1
@@ -16,6 +16,7 @@ while rollcounter < 10:
   else:
      print("Please enter Y or N to roll the dice")
 
+  break
 
 print("You have rolled the maximum number of times")
 
